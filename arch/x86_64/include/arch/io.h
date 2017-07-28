e86be25e934a536d75b9157373b489a45946ec93
Rostyslav <rostawesomegd@gmail.com>
2017-07-28 18:03:17 +0300

    Add 16-bit and 32-bit I/O support for x86_64 arch

--- arch/x86_64/include/arch/io.h
+++ arch/x86_64/include/arch/io.h
@@ -1,16 +1,40 @@
 #ifndef OTRIX_ARCH_IO_H
 #define OTRIX_ARCH_IO_H
 
-static inline uint8_t arch_io_read_byte(const uint16_t address)
+static inline uint8_t arch_io_read8(const uint16_t address)
 {
     uint8_t ret;
     asm volatile ("inb %%dx, %%al": "=a" (ret) : "d" (address));
     return ret;
 }
 
-static inline void arch_io_write_byte(const uint16_t address, const uint8_t data)
+static inline void arch_io_write8(const uint16_t address, const uint8_t data)
 {
     asm volatile ("outb %%al, %%dx": : "d" (address), "a" (data));
+}
+
+static inline uint16_t arch_io_read16(const uint16_t address)
+{
+    uint16_t ret;
+    asm volatile ("inb %%dx, %%ax": "=a" (ret) : "d" (address));
+    return ret;
+}
+
+static inline void arch_io_write16(const uint16_t address, const uint16_t data)
+{
+    asm volatile ("outb %%ax, %%dx": : "d" (address), "a" (data));
+}
+
+static inline uint16_t arch_io_read32(const uint16_t address)
+{
+    uint32_t ret;
+    asm volatile ("inb %%dx, %%eax": "=a" (ret) : "d" (address));
+    return ret;
+}
+
+static inline void arch_io_write32(const uint16_t address, const uint32_t data)
+{
+    asm volatile ("outb %%eax, %%dx": : "d" (address), "a" (data));
 }
 
 #endif // OTRIX_ARCH_IO_H
--- dev/serial.c
+++ dev/serial.c
@@ -31,26 +31,26 @@ enum serial_registers
 static void serial_set_baud(struct serial_dev *self)
 {
     // Enable DLAB
-    uint8_t line_ctl = arch_io_read_byte(self->com_port + SERIAL_LINE_CTL);
+    uint8_t line_ctl = arch_io_read8(self->com_port + SERIAL_LINE_CTL);
     line_ctl |= SERIAL_LINE_CTL_DLAB;
-    arch_io_write_byte(self->com_port + SERIAL_LINE_CTL, line_ctl);
+    arch_io_write8(self->com_port + SERIAL_LINE_CTL, line_ctl);
 
-    arch_io_write_byte(self->com_port + SERIAL_DATA, self->baud & 0xFF);
-    arch_io_write_byte(self->com_port + SERIAL_INTEN, self->baud & 0x00FF);
+    arch_io_write8(self->com_port + SERIAL_DATA, self->baud & 0xFF);
+    arch_io_write8(self->com_port + SERIAL_INTEN, self->baud & 0x00FF);
     // Disable DLAB
-    line_ctl = arch_io_read_byte(self->com_port + SERIAL_LINE_CTL);
+    line_ctl = arch_io_read8(self->com_port + SERIAL_LINE_CTL);
     line_ctl &= ~SERIAL_LINE_CTL_DLAB;
-    arch_io_write_byte(self->com_port + SERIAL_LINE_CTL, line_ctl);
+    arch_io_write8(self->com_port + SERIAL_LINE_CTL, line_ctl);
 }
 
 static bool serial_tx_ready(const struct serial_dev * const self)
 {
-    return arch_io_read_byte(self->com_port + SERIAL_LSR) & SERIAL_LSR_TX_EMPTY;
+    return arch_io_read8(self->com_port + SERIAL_LSR) & SERIAL_LSR_TX_EMPTY;
 }
 
 static bool serial_rx_ready(const struct serial_dev * const self)
 {
-    return arch_io_read_byte(self->com_port + SERIAL_LSR) & SERIAL_LSR_RX_READY;
+    return arch_io_read8(self->com_port + SERIAL_LSR) & SERIAL_LSR_RX_READY;
 }
 
 void serial_init(struct serial_dev *self,
@@ -59,21 +59,21 @@ void serial_init(struct serial_dev *self,
     self->com_port = com_port;
     self->baud = baud;
 
-    arch_io_write_byte(self->com_port + SERIAL_INTEN, 0x00); // Disable all interrupts
+    arch_io_write8(self->com_port + SERIAL_INTEN, 0x00); // Disable all interrupts
     serial_set_baud(self);
-    arch_io_write_byte(self->com_port, SERIAL_LINE_CTL_8BIT |
+    arch_io_write8(self->com_port, SERIAL_LINE_CTL_8BIT |
             SERIAL_LINE_CTL_1STOP_BIT | SERIAL_LINE_CTL_NO_PARITY);
-    arch_io_write_byte(self->com_port + SERIAL_IIR_FCR,
+    arch_io_write8(self->com_port + SERIAL_IIR_FCR,
             SERIAL_FCR_ENABLE_FIFO | SERIAL_FCR_CLEAR_RX_FIFO |
             SERIAL_FCR_CLEAR_TX_FIFO | SERIAL_FCR_14BYTES_FIFO);
-    arch_io_write_byte(self->com_port + SERIAL_MODEM_CTL, 0x00);
+    arch_io_write8(self->com_port + SERIAL_MODEM_CTL, 0x00);
 }
 
 void serial_write(const struct serial_dev * self, const uint8_t * const data, const size_t data_size)
 {
     for (size_t i = 0; i < data_size; i++) {
         while (!serial_tx_ready(self));
-        arch_io_write_byte(self->com_port + SERIAL_DATA, data[i]);
+        arch_io_write8(self->com_port + SERIAL_DATA, data[i]);
     }
 }
 
@@ -82,6 +82,6 @@ void serial_read(const struct serial_dev *self,
 {
     for (size_t i = 0; i < buffer_size; i++) {
         while (!serial_rx_ready(self));
-        buffer[i] = arch_io_read_byte(self->com_port + SERIAL_DATA);
+        buffer[i] = arch_io_read8(self->com_port + SERIAL_DATA);
     }
 }
