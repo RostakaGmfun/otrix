72d90aad809f819c571f1505ac58076c0ca69dd9
Rostyslav Kurylo <rostawesomegd@gmail.com>
2021-02-19 01:32:52 +0200

    Add network dump to run_qemu.sh

--- run_qemu.sh
+++ run_qemu.sh
@@ -3,4 +3,5 @@
 qemu-kvm -cdrom ./build/iso/otrix.iso -nographic \
          -device virtio-serial -chardev file,path=/tmp/otrix-log,id=otrix-log \
          -device virtconsole,name=jobsfoo,chardev=otrix-log,name=c \
-         -netdev user,id=n1 -device virtio-net-pci,netdev=n1
+         -netdev user,id=n1 -device virtio-net-pci,netdev=n1 \
+         -object filter-dump,id=f1,netdev=n1,file=net_out.pcap
